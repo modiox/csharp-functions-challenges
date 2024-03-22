71534f547c94610c844f7894b54a1fa412814b88
modiox <moodyalyalak@gmail.com>
2024-03-22 20:36:16 +0300

    Challenge 3 and 4

--- src/Program.cs
+++ src/Program.cs
@@ -1,28 +1,40 @@
 using System;
-using System.Linq.Expressions;
-using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+// using System.Linq.Expressions;
+// using System.Security.Cryptography;
 
 namespace FunctionChallenges
 {
     class Program
-    {
+    { 
 
+        
         //Function of Challenge 1
-        public static void StringNumberProcessor(params object[] inputs){
-        string concatenatedString = "";
+        static void StringNumberProcessor(params object[] inputs){
+        string concatenatedString = ""; 
+        List<string>  stringInputs = new List<string>();
         int sumOfNumbers = 0;
-
+     try { 
         foreach (var input in inputs)
-        {
+          {
             if (input is string)
             {
                 concatenatedString += input + " ";
+                stringInputs.Add((string)input);
+               
             }
             else if (input is int)
             {
                 sumOfNumbers += (int)input;
             }
+        } }
+        catch(Exception e)
+        { 
+            Console.WriteLine(e.Message);
         }
+
 
         // Trim white space from the string
         concatenatedString = concatenatedString.Trim();
@@ -32,23 +44,44 @@ namespace FunctionChallenges
     }
 
     //Function of Challenge 2 
-    //Description: The function should only swap objects if they are of the same type, either string or number. 
-    //For string, their lengths must be more than 5, and for number, they must be more than 18.
-    public static void SwapObjects(params object[] inputs) {
+    static void SwapObjects(ref object obj1, ref object obj2) {
 
+        if (obj1.GetType() != obj2.GetType()) { 
+    
+                Console.WriteLine("Objects must be of the same type.");
+                return;
+            }
 
-    }
+            if (obj1 is string && ((string)obj1).Length > 5 && ((string)obj2).Length > 5)
+            {
+                var temp = obj1;
+                obj1 = obj2;
+                obj2 = temp;
+            }
+            else if (obj1 is int && (int)obj1 > 18 && (int)obj2 > 18)
+            {
+                var temp = obj1;
+                obj1 = obj2;
+                obj2 = temp;
+            }
+            else
+            {
+                Console.WriteLine("Objects don't meet swapping conditions.");
+            }
+
+     }
+   
     //Function of Challenge 3
-    public static void GuessingGame()
-{
+    static void GuessingGame() {
     Random num = new Random();
+    Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("To play the Guessing Game...Enter a random number, and if you guess correctly you win :)");
-    Console.WriteLine("Type 'End' to quit the game..");
+    Console.WriteLine("Type 'End' to quit the game.."); 
+
 
     while (true)
     {
         string userInput = Console.ReadLine();
-
         if (userInput.ToLower() == "end")
         {
             Console.WriteLine("You've exited the game");
@@ -78,25 +111,52 @@ namespace FunctionChallenges
         {
             Console.WriteLine(e.Message);
         }
+    } }
+
+    //Function 4: Challenge 4
+      // Function 4: Challenge 4
+static string ReverseWords(string sentence)
+{
+    if (string.IsNullOrWhiteSpace(sentence))
+    {
+        throw new Exception("Invalid Input...");
     }
+
+    string[] sentenceArray = sentence.Split(' ');
+    List<string> reversedWords = new List<string>();
+
+    foreach (var str in sentenceArray)
+    {
+        string reversedWord = new string(str.Reverse().ToArray());
+        reversedWords.Add(reversedWord);
+    }
+
+    string result = string.Join(" ", reversedWords);
+    Console.WriteLine($"Result: {result}"); 
+    return result;
 }
 
-   
-
-        static void Main(string[] args)
+          static void Main(string[] args)
         {
             // Challenge 1: String and Number Processor
             Console.WriteLine("Challenge 1: String and Number Processor");
             StringNumberProcessor("Hello", 100, 200, "World"); // Expected outcome: "Hello World; 300"
             StringNumberProcessor("Hello", 100, 500, "World"); 
+            StringNumberProcessor("Hello", 100, "Hi", 500, 700, "World"); 
+
 
             // Challenge 2: Object Swapper
-            // Console.WriteLine("\nChallenge 2: Object Swapper");
-            // int num1 = 25, num2 = 30;
-            // int num 3 = 10, num4 = 30;
-            // string str1 = "HelloWorld", str2 = "Programming";
-            // string str3 = "Hi", str4 = "Programming";
-                          
+            Console.WriteLine("\nChallenge 2: Object Swapper");
+            int num1 = 25;
+            int num2 = 30;
+            // int num3 = 10, num4 = 30;
+            string str1 = "HelloWorld", str2 = "Programming";
+            string str3 = "Hi", str4 = "Programming";  
+            object objNum1 = num1;
+            object objNum2 = num2;
+            SwapObjects(ref objNum1, ref objNum2);
+            num1 = (int)objNum1;
+            num2 = (int)objNum2;      
             // SwapObjects(ref num1, ref num2); // Expected outcome: num1 = 30, num2 = 25  
             // SwapObjects(ref num3, ref num4); // Error: Value must be more than 18
 
@@ -106,19 +166,19 @@ namespace FunctionChallenges
             // SwapObjects(true, false); // Error: Upsupported data type
             // SwapObjects(ref num1, str1); // Error: Objects must be of same types
 
-            // Console.WriteLine($"Numbers: {num1}, {num2}");
-            // Console.WriteLine($"Strings: {str1}, {str2}");
+            Console.WriteLine($"Numbers: {num1}, {num2}");
+            Console.WriteLine($"Strings: {str1}, {str2}");
 
             // Challenge 3: Guessing Game
             Console.WriteLine("\nChallenge 3: Guessing Game");
             // Uncomment to test the GuessingGame method
              GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
 
-            // // Challenge 4: Simple Word Reversal
-            // Console.WriteLine("\nChallenge 4: Simple Word Reversal");
-            // string sentence = "This is the original sentence!";
-            // string reversed = ReverseWords(sentence);
-            // Console.WriteLine(reversed); // Expected outcome: "sihT si eht lanigiro !ecnetnes"
+            // Challenge 4: Simple Word Reversal
+            Console.WriteLine("\nChallenge 4: Simple Word Reversal");
+            string sentence = "This is the original sentence!";
+            string reversed = ReverseWords(sentence);
+            Console.WriteLine(reversed); // Expected outcome: "sihT si eht lanigiro !ecnetnes"
         }
-    }
-}
+    } } 
+
